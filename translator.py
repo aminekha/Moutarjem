4bf45d9d645ef81966c74a58793a896dd1b42699
aminekh <aminekhaoui97@gmail.com>
2019-08-26 12:50:11 +0100

    Adding voice cloning files

--- translator.py
+++ translator.py
@@ -2,7 +2,7 @@
 This file will translate any text from english to french
 """
 import torch
-# import fairseq
+import fairseq
 
 # List available models
 torch.hub.list('pytorch/fairseq')  # [..., 'transformer.wmt16.en-de', ... ]
@@ -15,5 +15,6 @@ assert isinstance(en2de.models[0], fairseq.models.transformer.TransformerModel)
 
 # Translate a sentence
 text_to_translate = "Dear Vneuron, My name is Amine Khaoui. I am incredibly passionate about all things technology, artificial intelligence, and data. Over the past 2 years, I have dedicated a considerable amount of time and effort to master machine learning algorithms, It amazed me and I have been dedicating my education to that ever since. "
-output = en2de.translate("hello, world")
+text = input("> ")
+output = en2de.translate(text)
 print(output)
